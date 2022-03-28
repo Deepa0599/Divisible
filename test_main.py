db4741821d0e1cce88cdfcded3d0cd73947f4d91
Deepa0599 <gadhavedeepa07@gmail.com>
2022-03-28 17:28:41 +0530

    Complete

--- test_main.py
+++ test_main.py
@@ -1,43 +1,48 @@
 import main
+import pytest
 
-def test_divby5_1():
-    a = 10
-    out1 = main.divby5(a)
+@pytest.fixture
+def input():
+    a = 315
+    return a
+def test_divby5_1(input):
+    # a = 10
+    out1 = main.divby5(input)
     assert out1 == True
 
-def test_divby5_2():
-    a = 11
-    out1 = main.divby5(a)
+def test_divby5_2(input):
+    #a = 11
+    out1 = main.divby5(input)
     assert out1 == False
 
-def test_divby7_1():
-    a = 14
-    out1 = main.divby7(a)
+def test_divby7_1(input):
+    #a = 14
+    out1 = main.divby7(input)
     assert out1 == True
 
-def test_divby7_2():
-    a = 15
-    out1 = main.divby7(a)
+def test_divby7_2(input):
+   # a = 15
+    out1 = main.divby7(input)
     assert out1 == False
 
-def test_divby9_1():
-    a = 18
-    out1 = main.divby9(a)
+def test_divby9_1(input):
+   # a = 18
+    out1 = main.divby9(input)
     assert out1 == True
 
 def test_divby9_2():
-    a = 19
-    out1 = main.divby9(a)
+  #  a = 19
+    out1 = main.divby9(input)
     assert out1 == False
 
-def test_divby11_1():
-    a = 22
-    out1 = main.divby11(a)
+def test_divby11_1(input):
+   # a = 22
+    out1 = main.divby11(input)
     assert out1 == True
 
-def test_divby11_2():
-    a = 23
-    out1 = main.divby11(a)
+def test_divby11_2(input):
+    #a = 23
+    out1 = main.divby11(input)
     assert out1 == False
 
 
